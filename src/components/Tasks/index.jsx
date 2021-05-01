b81c7898aa41bfc57f73932f18d29782345ccf2f
GorohovDmitriy <dmitriy2893@gmail.com>
2021-05-01 13:00:27 +0300

    first commit

---
+++ src/components/Tasks/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import editSvg from '../../assets/img/edit.svg';
+import Task from './Task';
+import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+
+
+import './Tasks.scss';
+import AddTaskForm from './AddTaskForm';
+
+const Tasks = ({ list, onEditTitle, onAddTask, onRemoveTask, withoutEmpty, onComplateTask, onEditTask }) => {
+
+	const editTitle = () => {
+		const newTitle = window.prompt('Название списка', list.name);
+		if (newTitle) {
+			onEditTitle(list.id, newTitle);
+			axios.patch('http://localhost:3001/lists/' + list.id, {
+				name: newTitle
+			}).catch(() => {
+				alert('Не удалось обновить название списка');
+			});
+		}
+	}
+
+
+
+	return (
+		<div className="tasks">
+			<Link to={`/lists/${list.id}`}>
+				<h2 style={{ color: list.color.hex }} className="tasks__title">
+					{list.name}
+					<img onClick={editTitle}
+						src={editSvg}
+						alt="Edit icon"
+					/>
+				</h2>
+			</Link>
+			<div className="tasks__items">
+				{!withoutEmpty && list.tasks && !list.tasks.length && <h2> Задачи отсутствуют </h2>}
+				{list.tasks && list.tasks.map(task =>
+					<Task key={task.id} list={list} onEdit={onEditTask} onRemove={onRemoveTask} onComplete={onComplateTask} {...task} />
+				)}
+				<AddTaskForm key={list.id} list={list} onAddTask={onAddTask} />
+			</div>
+		</div>
+	);
+};
+
+export default Tasks;
